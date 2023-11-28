1212cd146c1a4382c904f053a09866a1c6365f1a
Ada Rautiainen <ada.rautiainen@gmail.com>
2023-11-28 10:16:57 +0200

    pylint

--- src/bibtex_utils.py
+++ src/bibtex_utils.py
@@ -1,6 +1,7 @@
 def get_reference_type(print_message=True):
     while True:
-        reference_type = input("Enter reference type (article, book, inproceedings) or exit: ").lower()
+        reference_type = input("Enter reference type (article, book, inproceedings)"
+                               " or  exit: ").lower()
         if reference_type == "exit":
             return None
         if reference_type in ['article', 'book', 'inproceedings']:
@@ -10,7 +11,7 @@ def get_reference_type(print_message=True):
 
 
 def ask_fields(reference_type):
-    reference_key = input("Enter key for reference: ")  # lähdeviitteen ensimmäinen kenttä @...
+    reference_key = input("Enter key for reference: ")
     while not reference_key.strip():
         print("Cannot be empty")
         reference_key = input("Enter key for reference: ")
@@ -24,14 +25,15 @@ def ask_fields(reference_type):
     }
 
     for field in required_fields.get(reference_type, []):
-        while True:
+        valid_input = False
+        while not valid_input:
             user_input = input(f"Enter {field}: ")
             if user_input.strip():
                 if field == 'year' and not user_input.isdigit():
                     print("Invalid input")
                 else:
                     fields[field] = user_input
-                    break
+                    valid_input = True
             else:
                 print("Field cannot be empty")
 
