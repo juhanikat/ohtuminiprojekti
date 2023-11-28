a8a92ada538b6164f2cb25e80c2d5009b275c1d1
Ada Rautiainen <ada.rautiainen@gmail.com>
2023-11-28 10:25:59 +0200

    pylint corrected

--- src/bibtex_utils.py
+++ src/bibtex_utils.py
@@ -1,23 +1,21 @@
-def get_reference_type(print_message=True):
+def get_reference_type():
     while True:
-        reference_type = input("Enter reference type (article, book, inproceedings)"
-                               " or  exit: ").lower()
+        reference_type = input("Enter reference type (article, book,"
+                               " inproceedings)or  exit: ").lower()
         if reference_type == "exit":
             return None
         if reference_type in ['article', 'book', 'inproceedings']:
             return reference_type
-        if print_message:
-            print("Invalid type")
+        print("Invalid type")
 
 
 def ask_fields(reference_type):
-    reference_key = input("Enter key for reference: ")
-    while not reference_key.strip():
+    reference_key = input("Enter key for reference: ").strip()
+    while not reference_key:
         print("Cannot be empty")
-        reference_key = input("Enter key for reference: ")
+        reference_key = input("Enter key for reference: ").strip()
 
     fields = {}
-
     required_fields = {
         'article': ['author', 'title', 'journal', 'year', 'volume'],
         'book': ['author', 'title', 'publisher', 'year'],
@@ -25,16 +23,14 @@ def ask_fields(reference_type):
     }
 
     for field in required_fields.get(reference_type, []):
-        valid_input = False
-        while not valid_input:
-            user_input = input(f"Enter {field}: ")
-            if user_input.strip():
-                if field == 'year' and not user_input.isdigit():
-                    print("Invalid input")
-                else:
-                    fields[field] = user_input
-                    valid_input = True
-            else:
-                print("Field cannot be empty")
+        user_input = input(f"Enter {field}: ").strip()
+        while not user_input:
+            print("Field cannot be empty")
+            user_input = input(f"Enter {field}: ").strip()
+
+        if field == 'year' and not user_input.isdigit():
+            print("Invalid input")
+        else:
+            fields[field] = user_input
 
     return reference_key, fields
